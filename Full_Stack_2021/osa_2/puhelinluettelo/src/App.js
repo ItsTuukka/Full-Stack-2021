7fe9c323f931abc61a79ab531b8b80365333d2a2
ItsTuukka <tuukka.ylostalo@helsinki.fi>
2022-01-31 20:01:26 +0200

    2.11

--- Full_Stack_2021/osa_2/puhelinluettelo/src/App.js
+++ Full_Stack_2021/osa_2/puhelinluettelo/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Form from './components/Form'
 import ShowNumbers from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
   const names = persons.map(person => person.name)
+
+  useEffect(() => {
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response =>{
+      setPersons(response.data)
+    })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
