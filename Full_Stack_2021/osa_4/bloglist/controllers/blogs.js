23b0762a90b23dd46c43152b9a264c5a74856814
ItsTuukka <tuukka.ylostalo@helsinki.fi>
2022-02-12 00:01:43 +0200

    4.8-4.14

--- Full_Stack_2021/osa_4/bloglist/controllers/blogs.js
+++ Full_Stack_2021/osa_4/bloglist/controllers/blogs.js
@@ -1,22 +1,49 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
 })
   
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
+blogsRouter.post('/', async (request, response) => {
+    const data = request.body
+    if (!data.title || !data.url) {
+        return response.status(400).json({
+            error: 'missing title or url'
         })
+    }
+    const blog = new Blog({
+        title: data.title,
+        author: data.author,
+        url: data.url,
+        likes: data.likes || 0
+    })
+  
+    const savedBlog = await blog.save()
+    response.json(savedBlog.toJSON())
 })
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+    const body = request.body
+  
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0
+    }
+  
+    Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+      .then(updatedBlog => {
+        response.json(updatedBlog)
+      })
+      .catch(error => next(error))
+  })
 
 module.exports = blogsRouter
---
+++ Full_Stack_2021/osa_4/bloglist/tests/blogs_api.test.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const { expect } = require('@jest/globals')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('blog tests', () => {
+    test('blogs are returned as json', async () => {
+        await api
+          .get('/api/blogs')
+          .expect(200)
+          .expect('Content-Type', /application\/json/)
+      })
+    
+    test('all blogs are returned', async () => {
+        const res = await api.get('/api/blogs')
+
+        expect(res.body).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('blogs have id field', async () => {
+        const res = await api.get('/api/blogs')
+
+        expect(res.body[0].id).toBeDefined()
+    })
+
+    test('add blog', async () => {
+        const newBlog = {
+            title: "dafag", 
+            author: "markus", 
+            url: "nojaa.com", 
+            likes: 69
+        }
+
+        await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+        const uptatedBlogs = await helper.blogsInDb()
+        expect(uptatedBlogs).toHaveLength(helper.initialBlogs.length + 1)
+
+        const authors = uptatedBlogs.map(b => b.author)
+        expect(authors).toContain('markus')
+    })
+
+    test('add blog withou like param, check if 0', async () => {
+        const newBlog = {
+            title: "dafag", 
+            author: "markus", 
+            url: "nojaa.com"
+        }
+
+        const res = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+        const uptatedBlogs = await helper.blogsInDb()
+        expect(uptatedBlogs).toHaveLength(helper.initialBlogs.length + 1)
+
+        expect(res.body.likes).toBe(0)
+    })
+
+    test('get 400 if invalid data', async () => {
+        const newBlog = {
+            author: "markus", 
+            likes: 12
+        }
+
+        await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+        const uptatedBlogs = await helper.blogsInDb()
+        expect(uptatedBlogs).toHaveLength(helper.initialBlogs.length)
+    })
+})
+
+
+afterAll(() => {
+  mongoose.connection.close()
+})
